4b7dddf8cf6df93a8da3aae75a9feef76b8d99b0
Parker-Caitlin <caitlin.parker@my.jcu.edu.au>
2017-08-17 09:30:18 +1000

    Add Starter File

--- oddName.py
+++ oddName.py
@@ -2,3 +2,10 @@
 Prac 3- CP1404
 Caitlin Parker
 """
+
+user_name = input("Enter Name: ")
+check_len = len(user_name)
+while check_len <= 0:
+    user_name = input("Error. Input Name: ")
+    check_len = len(user_name)
+print(user_name[1::2])
